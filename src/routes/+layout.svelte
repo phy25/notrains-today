ca6b278834329c4797303102375c16f579a083d6
Phy <git@phy25.com>
2025-03-18 01:23:54 -0400

    Initial sv commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">	
+	// import { i18n } from '$lib/i18n';
+	// import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	let { children } = $props();
+</script>
+
+{@render children()}
---
+++ src/routes/demo/paraglide/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { localizeHref, deLocalizeHref, setLocale, type Locale } from "$lib/paraglide/runtime";
+	import { page } from '$app/state';
+	import { goto } from '$app/navigation';
+	import * as m from '$lib/paraglide/messages';
+
+	function switchToLanguage(newLanguage: Locale) {
+		setLocale(newLanguage);
+		return;
+		const canonicalPath = deLocalizeHref(page.url.pathname);
+		const localisedPath = localizeHref(canonicalPath, {locale: newLanguage});
+		goto(localisedPath);
+	}
+</script>
+
+<h1>{m.hello_world({ name: 'SvelteKit User' })}</h1>
+<div>
+	<button onclick={() => switchToLanguage('en')}>en</button>
+	<button onclick={() => switchToLanguage('zh-hans')}>zh-hans</button>
+</div>
