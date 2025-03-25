d07e9cd2507949cce4ec635aeb9a8f1c0456f491
Phy <git@phy25.com>
2025-03-25 00:22:29 -0400

    split alarts with multiple routes in calendar view

--- src/lib/calendar.ts
+++ src/lib/calendar.ts
@@ -4,31 +4,63 @@ export const MBTA_SERVICE_START_HOUR = 3;
 // usually, end time is 2:59 and start time is 3:00
 // we have also seen buses service hour on 3:30
 
+const insertSingleAlert = (alert: MbtaAlert, days: Map<string, MbtaAlert[]>, filterRoute?: string) => {
+    alert.attributes.active_period.forEach((period) => {
+        if (!period.start || !period.end) {
+            // TODO: what to do with ferry alerts with no end time
+            return;
+        }
+        const startDate = new Date(period.start);
+        const endDate = new Date(period.end);
+        const currentDate = new Date(startDate);
+        // only when the change is after MBTA_SERVICE_START_HOUR we mark it on that day
+        currentDate.setHours(MBTA_SERVICE_START_HOUR, 30, 0, 0);
+
+        while (currentDate <= endDate) {
+            const dateString = getDateString(currentDate);
+            if (!days.has(dateString)) {
+                days.set(dateString, []);
+            }
+            if (!filterRoute) {
+                days.get(dateString)?.push(alert);
+            } else {
+                days.get(dateString)?.push({
+                    ...alert,
+                    attributes: {
+                        ...alert.attributes,
+                        informed_entity: alert.attributes.informed_entity.filter(
+                            (entity) => entity.route === filterRoute),
+                    }
+                });
+            }
+            
+
+            // check next day MBTA_SERVICE_START_HOUR
+            currentDate.setDate(currentDate.getDate() + 1);
+        }
+    });
+}
+
 export const getAlertsAsDays = (alerts: MbtaAlert[], routeMap: Map<string, any>) => {
     const days: Map<string, MbtaAlert[]> = new Map();
     alerts.forEach((alert) => {
-        alert.attributes.active_period.forEach((period) => {
-            if (!period.start || !period.end) {
-                // TODO: what to do with ferry alerts with no end time
-                return;
-            }
-            const startDate = new Date(period.start);
-            const endDate = new Date(period.end);
-            const currentDate = new Date(startDate);
-            // only when the change is after MBTA_SERVICE_START_HOUR we mark it on that day
-            currentDate.setHours(MBTA_SERVICE_START_HOUR, 30, 0, 0);
+        // if the alert impacts multiple routes, split them into multiple alerts
+        const uniqueRoutes = alert.attributes?.informed_entity
+            ?.map(entity => entity.route)
+            ?.filter(
+                // get unique route
+                (route, index, arr) => route && arr.indexOf(route) === index
+            );
+        
+        if (uniqueRoutes && uniqueRoutes.length > 0) {
+            uniqueRoutes.forEach((route) => {
+                insertSingleAlert(alert, days, route);
+            });
+        } else {
+            insertSingleAlert(alert, days);
+        }
 
-            while (currentDate <= endDate) {
-                const dateString = getDateString(currentDate);
-                if (!days.has(dateString)) {
-                    days.set(dateString, []);
-                }
-                days.get(dateString)?.push(alert);
-
-                // check next day MBTA_SERVICE_START_HOUR
-                currentDate.setDate(currentDate.getDate() + 1);
-            }
-        });
+        
     });
     // sort alerts
     days.forEach((alerts, _) => {
