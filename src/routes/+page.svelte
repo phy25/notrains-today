9384642a443e848ddc21aa0f9d94a6fcce6493a3
Phy <git@phy25.com>
2025-04-09 01:59:36 -0400

    Add refresh reminder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
 	import PageAsync from './page-async.svelte';
+	import { invalidateAll } from '$app/navigation';
+	import { m } from '$lib/paraglide/messages';
 
 	const { data }: PageProps = $props();
+    let isOutdated = $state(false);
+    $effect(() => {
+        data.data_async;
+        setTimeout(() => {
+            isOutdated = true;
+        }, 1000 * 60 * 5);
+    });
 </script>
+
+{#if isOutdated}
+    <div class="alert alert-warning">
+        <p><button onclick={() => {isOutdated = false;invalidateAll();}}>{m.refresh_reminder()}</button></p>
+    </div>
+{/if}
 
 {#await data.data_async() then d}
 	<PageAsync
