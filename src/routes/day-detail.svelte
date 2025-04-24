4f3e4f7fdcb3fcffab9a4bd92df086aa2a172303
Phy <git@phy25.com>
2025-04-24 01:53:50 -0400

    Support basic mult-route rendering with day-detail

--- src/routes/day-detail.svelte
+++ src/routes/day-detail.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 import { getDateString, MBTA_SERVICE_START_HOUR } from "$lib/calendar";
-import { EFFECT_MESSAGES, getEffectWithLineMessage, getPillName } from "$lib/mbta-display";
+import { EFFECT_MESSAGES, getEffect, getEffectWithLineMessage, getPillName } from "$lib/mbta-display";
 import { MBTA_TIMEZONE, type MbtaAlert } from "$lib/mbta-types";
 import { m } from "$lib/paraglide/messages";
 import { getLocale } from "$lib/paraglide/runtime";
@@ -8,7 +8,13 @@ import { parseDate, DateFormatter } from "@internationalized/date";
 import { fade } from "$lib/transition";
 import MbtaRouteBadgeCompound from "$lib/mbta-route-badge-compound.svelte";
 
-const { day, alerts, showNightOwl, routeMap, hideAuxiliary = false } = $props();
+const { day, alerts, showNightOwl, routeMap, hideAuxiliary = false }: {
+    day: string;
+    alerts: MbtaAlert[];
+    showNightOwl: boolean;
+    routeMap: Map<string, any>;
+    hideAuxiliary?: boolean;
+} = $props();
 
 const dayObject = $derived(parseDate(day));
 const dateFormatter = new DateFormatter(getLocale());
@@ -25,14 +31,25 @@ if (showNightOwl) {
 
 {#each alerts as alert}
     {@const effect = alert.attributes.effect as keyof typeof EFFECT_MESSAGES}
-    {@const route_id = (alert as MbtaAlert).attributes.informed_entity.sort((a, b) => a.route.localeCompare(b.route))[0].route /* TODO: show multiple routes */ }
-    {@const attributes = (routeMap.get(route_id) as any)?.attributes}
+    {@const unique_routes = alert.attributes.informed_entity.map(entity => entity.route).filter((value, index, self) => self.indexOf(value) === index).sort((a, b) => a.localeCompare(b))}
     {@const descriptionArr = alert.attributes?.description?.split(/\r?\n/g) || []}
     <details transition:fade id={'alert-'+alert.id}>
         <summary>
             <!-- remove <p> to work with the marker. Temporary anyway. -->
-            <span class="badge-group"><MbtaRouteBadgeCompound type="long" routeId={route_id} routeAttributes={attributes} /></span>
-            <span class="alert-title">{getEffectWithLineMessage(effect, route_id, attributes)}</span>
+            {#if unique_routes.length == 1}
+                {@const route_id = unique_routes[0]}
+                {@const attributes = (routeMap.get(route_id) as any)?.attributes}
+                <span class="badge-group"><MbtaRouteBadgeCompound type="long" routeId={route_id} routeAttributes={attributes} /></span>
+                <span class="alert-title">{getEffectWithLineMessage(effect, route_id, attributes)}</span>
+            {:else}
+                <div class="badge-groups">
+                    {#each unique_routes as route_id}
+                        {@const attributes = (routeMap.get(route_id) as any)?.attributes}
+                        <span class="badge-group"><MbtaRouteBadgeCompound type="long" routeId={route_id} routeAttributes={attributes} /></span>
+                    {/each}
+                </div>
+                <span class="alert-title">{getEffect(effect)}</span>
+            {/if}
             <p>
                 {alert.attributes.header}
             </p>
@@ -59,7 +76,7 @@ if (showNightOwl) {
         <p><em>{m.learnMoreAt()}<a href={alert.attributes?.url} target="_blank">{alert.attributes?.url}</a> ({m.alert()} #{alert.id})</em></p>
         {:else}
         
-        <p><em><a href="https://www.mbta.com/schedules/{route_id}/alerts" target="_blank">{m.alert()} #{alert.id}</a></em></p>
+        <p><em><a href="https://www.mbta.com/schedules/{unique_routes[0]}/alerts" target="_blank">{m.alert()} #{alert.id}</a></em></p>
         {/if}
 
     </details>
@@ -75,6 +92,10 @@ h2 > small {
 }
 summary {
     --badge-size: 1.3em;
+}
+summary .badge-groups {
+    display: inline-flex;
+    gap: 0.3em;
 }
 summary .badge-group {
     user-select: none;
