31bc8accdeda401951f3b1c60a315e49cd9c79a7
Phy <git@phy25.com>
2025-06-23 22:53:43 -0400

    Add lastUpdatedAt in dayDetail, fix night owl calculation

--- src/lib/calendar.ts
+++ src/lib/calendar.ts
@@ -150,7 +150,11 @@ export const getAlertsMapByEffect = (alerts: MbtaAlert[]) => {
 }
 
 export const getDateString = (date: Date) => {
-    return date.toISOString().split('T')[0];
+    // get local time string in YYYY-MM-DD format
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
 };
 
 export const alertsToRouteRenderingList = (alerts: MbtaAlert[], routeMap: Map<string, any>) => {
--- src/routes/day-detail.svelte
+++ src/routes/day-detail.svelte
@@ -4,7 +4,7 @@ import { EFFECT_MESSAGES, getEffect, getEffectWithLineMessage, getPillName } fro
 import { MBTA_TIMEZONE, type MbtaAlert } from "$lib/mbta-types";
 import { m } from "$lib/paraglide/messages";
 import { getLocale } from "$lib/paraglide/runtime";
-import { parseDate, DateFormatter } from "@internationalized/date";
+import { parseDate, DateFormatter, fromDate, toCalendarDate, parseZonedDateTime } from "@internationalized/date";
 import { fade } from "$lib/transition";
 import MbtaRouteBadgeCompound from "$lib/mbta-route-badge-compound.svelte";
 
@@ -24,19 +24,20 @@ const dateFormatter = new DateFormatter(getLocale(), {
 });
 const currentTime = new Date();
 const currentServiceDate = new Date(currentTime);
-if (showNightOwl) {
+if (showNightOwl && currentTime.getHours() < MBTA_SERVICE_START_HOUR) {
     currentServiceDate.setDate(currentServiceDate.getDate() - 1);
 }
 </script>
 
 {#if !hideAuxiliary}
-<h2>{dateFormatter.format(dayObject.toDate(MBTA_TIMEZONE))}{#if showNightOwl && day == getDateString(currentServiceDate)}<small>{m.toServiceEndingNightOwl({hour: MBTA_SERVICE_START_HOUR})}</small>{/if}</h2>
+<h2>{dateFormatter.format(dayObject.toDate(MBTA_TIMEZONE))}{#if showNightOwl && day == getDateString(currentServiceDate) && currentTime.getHours() < MBTA_SERVICE_START_HOUR}<small>{m.toServiceEndingNightOwl({hour: MBTA_SERVICE_START_HOUR})}</small>{/if}</h2>
 {/if}
 
 {#each alerts || [] as alert}
     {@const effect = alert.attributes.effect as keyof typeof EFFECT_MESSAGES}
     {@const unique_routes = alert.attributes.informed_entity.map(entity => entity.route).filter((value, index, self) => self.indexOf(value) === index).sort((a, b) => a.localeCompare(b))}
     {@const descriptionArr = alert.attributes?.description?.split(/\r?\n/g) || []}
+    {@const updatedAtDate = parseZonedDateTime(alert.attributes.updated_at + '[' + MBTA_TIMEZONE + ']').toDate()}
     <details transition:fade id={'alert-'+alert.id}>
         <summary>
             <!-- remove <p> to work with the marker. Temporary anyway. -->
@@ -75,11 +76,26 @@ if (showNightOwl) {
             {/each}
         </p>
         {/if}
+        <p></p>
 
         {#if alert.attributes?.url}
-        <p><em>{m.learnMoreAt()}<a href={alert.attributes?.url} target="_blank">{alert.attributes?.url}</a> ({m.alert()} #{alert.id})</em></p>
+        <p><em>
+            {m.learnMoreAt()}<a href={alert.attributes?.url} target="_blank">{alert.attributes?.url}</a> ({m.alert()} #{alert.id}); 
+                {m.lastUpdatedAtTime({time: new DateFormatter(getLocale(), {
+                dateStyle: getDateString(updatedAtDate) == getDateString(currentServiceDate) ? undefined : "short",
+                timeStyle: "short",
+                timeZone: MBTA_TIMEZONE,
+            }).format(updatedAtDate)})}
+        </em></p>
         {:else}
-        <p><em><a href="https://www.mbta.com/schedules/{unique_routes[0]}/alerts" target="_blank">{m.alert()} #{alert.id}</a></em></p>
+        <p><em>
+            <a href="https://www.mbta.com/schedules/{unique_routes[0]}/alerts" target="_blank">{m.alert()} #{alert.id}</a>; 
+            {m.lastUpdatedAtTime({time: new DateFormatter(getLocale(), {
+                dateStyle: getDateString(updatedAtDate) == getDateString(currentServiceDate) ? undefined : "short",
+                timeStyle: "short",
+                timeZone: MBTA_TIMEZONE,
+            }).format(updatedAtDate)})}
+        </em></p>
         {/if}
 
     </details>
